f32439cd90945f0d69f77fd3f0138588043c783c
hlebon <hanslgarcia@gmail.com>
2017-12-06 17:50:58 -0500

    feat: adding templates for post, graphql queries and more.

---
+++ src/pages/2017-12-06/index.md
@@ -0,0 +1,10 @@
+---
+path: "/common-day"
+date: "2017-12-06-31T4:07:55"
+title: "A common day, today"
+tags: ['navidad', 'blog', 'cool']
+excerpt: "Hello World my first post"
+---
+
+#a common day , today
+I dont know what to write... 
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,15 +1,50 @@
 import React from 'react'
 import Link from 'gatsby-link'
+import { graphql } from 'graphql';
 
-const IndexPage = () => (
-  <div style={{color: 'pink'}}>
+const IndexPage = ({data}) => {
+  const { edges: posts } = data.allMarkdownRemark
+  return (
+    <div style={{color: 'pink'}}>
     <h1>Hi people</h1>
     <p>Hello world!</p>
-    <img src="http://lorempixel.com/400/200/" alt="" />
     <p>This is my first gatsby page</p>
-    <Link to="/page-2/">Go to page 2</Link>
-    <Link to="/page-3/">Go to page 3</Link>
+    <ul>
+      {posts.map(({node: post}) => {
+        const { frontmatter } = post
+        return (
+          <div>
+            <h3>
+              <Link to={frontmatter.path}>{frontmatter.title}</Link>
+            </h3>
+            <p>{frontmatter.date}</p>
+            <p>{frontmatter.excerpt}</p>
+          </div>
+        )
+      })}
+    </ul>
   </div>
-)
+  )
+}
+
+export const query = graphql`
+  query IndexQuery {
+    allMarkdownRemark {
+      totalCount
+      edges{
+        node{
+          id
+          frontmatter{
+            title
+            date
+            path
+            tags
+            excerpt
+          }
+        }
+      }
+    }
+  }
+`
 
 export default IndexPage
