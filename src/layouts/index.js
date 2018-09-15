b943d27761baf1592ab5e1dd6671849533aff8fe
hlebon <hanslgarcia@gmail.com>
2018-09-15 11:04:01 -0500

    feat: new classnames and icons in library

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -2,17 +2,17 @@ import React from "react";
 import Helmet from "react-helmet";
 import { library } from "@fortawesome/fontawesome-svg-core";
 import { fab } from "@fortawesome/free-brands-svg-icons";
-import { faCheckSquare, faCoffee } from "@fortawesome/free-solid-svg-icons";
+import { faFutbol, faCoffee } from "@fortawesome/free-solid-svg-icons";
 import { Nav, Footer } from "../components";
 import "./normalize.css";
 import "./index.css";
 import "./app.css";
 import "./prims-funky.css";
 
-library.add(fab, faCheckSquare, faCoffee);
+library.add(fab, faCoffee, faFutbol);
 
 const TemplateWrapper = ({ children }) => (
-  <div>
+  <div className="site">
     <Helmet
       title="Hans blog"
       meta={[
@@ -23,8 +23,10 @@ const TemplateWrapper = ({ children }) => (
         }
       ]}
     />
-    <Nav />
-    {children()}
+    <div className="content-site">
+      <Nav />
+      {children()}
+    </div>
     <Footer />
   </div>
 );
