e149358e3bb38c1c92aa809b75a1d8060d54737f
hlebon <hanslgarcia@gmail.com>
2018-09-15 07:10:42 -0500

    feat: adding fontawesome library

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -1,24 +1,32 @@
-import React from 'react';
-import Helmet from 'react-helmet';
+import React from "react";
+import Helmet from "react-helmet";
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { fab } from "@fortawesome/free-brands-svg-icons";
+import { faCheckSquare, faCoffee } from "@fortawesome/free-solid-svg-icons";
 import { Nav, Footer } from "../components";
 import "./normalize.css";
-import './index.css';
-import './app.css';
+import "./index.css";
+import "./app.css";
 import "./prims-funky.css";
+
+library.add(fab, faCheckSquare, faCoffee);
 
 const TemplateWrapper = ({ children }) => (
   <div>
     <Helmet
       title="Hans blog"
       meta={[
-        { name: 'description', content: 'Fullstack developer' },
-        { name: 'keywords', content: 'c#, javascript, reactjs, MVC, asp.net, entity framework' },
+        { name: "description", content: "Fullstack developer" },
+        {
+          name: "keywords",
+          content: "c#, javascript, reactjs, MVC, asp.net, entity framework"
+        }
       ]}
     />
     <Nav />
-      {children()}
-    <Footer/>
+    {children()}
+    <Footer />
   </div>
-)
+);
 
-export default TemplateWrapper
+export default TemplateWrapper;
