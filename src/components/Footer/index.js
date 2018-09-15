0f71920cd788c8273e8c6ca5dde5fc239e468346
hlebon <hanslgarcia@gmail.com>
2018-09-15 07:10:15 -0500

    feat: footer section

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,19 +1,31 @@
-import React from 'react';
+import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import "../../layouts/footer.css";
 
-function Footer(){
-    return(
-       <footer>
-           <section>
-               <div>&copy; Hans Garcia 2018</div>
-               <div>
-                   <ul>
-                       <li>Twitter</li>
-                       <li>Medium</li>
-                   </ul>
-               </div>
-           </section>
-       </footer>
-    )
+function Footer() {
+  return (
+    <footer className="footer-page">
+      <div className="footer-page-container flex">
+        <section className="footer-page-iam">
+          <h4 className="title">Hans Garcia</h4>
+          <p>
+            Software web developer, amante del cafe, la pizza y fan del A.C.
+            Milan.
+          </p>
+        </section>
+        <div>
+          <ul className="follow-me-list">
+            <li>
+              Twitter: <FontAwesomeIcon icon={["fab", "twitter"]} />
+            </li>
+            <li>
+              Medium: <FontAwesomeIcon icon={["fab", "medium"]} />
+            </li>
+          </ul>
+        </div>
+      </div>
+    </footer>
+  );
 }
 
 export default Footer;
