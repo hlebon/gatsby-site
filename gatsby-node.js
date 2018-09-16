c6d7c13a21f7f405eabe6237e2749abf8498653a
hlebon <hanslgarcia@gmail.com>
2018-09-16 00:46:47 -0500

    feat: new config for post and tags

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,93 +1,88 @@
-const path = require('path')
+const path = require("path");
 
 const createTagPages = (createPage, posts) => {
-    const tagPageTemplate = path.resolve(`src/templates/tags.js`)
-    const allTagsTemplate = path.resolve(`src/templates/all-tags.js`)
+  const tagPageTemplate = path.resolve(`src/templates/tags.js`);
+  const allTagsTemplate = path.resolve(`src/templates/all-tags.js`);
 
-    const postsByTags = {}
-    
-    posts.forEach(({node}) => {
-        if(node.frontmatter.tags){
-            node.frontmatter.tags.forEach(tag => {
-                if(!postsByTags[tag]){
-                    postsByTags[tag] = []
-                }
-    
-                postsByTags[tag].push(node)
-            })
+  const postsByTags = {};
+
+  posts.forEach(({ node }) => {
+    if (node.tags) {
+      node.tags.forEach(tag => {
+        if (!postsByTags[tag]) {
+          postsByTags[tag] = [];
         }
-    })
 
-    const tags = Object.keys(postsByTags)
+        postsByTags[tag].push(node);
+      });
+    }
+  });
+
+  const tags = Object.keys(postsByTags);
+  createPage({
+    path: `/tags`,
+    component: allTagsTemplate,
+    context: {
+      tags: tags.sort()
+    }
+  });
+
+  tags.forEach(tagName => {
+    const posts = postsByTags[tagName];
 
     createPage({
-        path: `/tags`,
-        component: allTagsTemplate,
-        context: {
-            tags: tags.sort()
-        }
-    })
-
-    tags.forEach(tagName => {
-        const posts = postsByTags[tagName]
-
-        createPage({
-            path: `/tags/${tagName}`,
-            component: tagPageTemplate,
-            context: {
-                posts,
-                tagName
-            }
-        })
-    })
-}
-
-
+      path: `/tags/${tagName}`,
+      component: tagPageTemplate,
+      context: {
+        posts,
+        tagName
+      }
+    });
+  });
+};
 
 exports.createPages = ({ boundActionCreators, graphql }) => {
-    const { createPage } = boundActionCreators
-    const blogPostTemplate = path.resolve(`src/templates/blog-post.js`)
+  const { createPage } = boundActionCreators;
 
-    const gql = graphql(`{
-        allMarkdownRemark {
-            edges {
-                node {
-                    html,
-                    id,
-                    frontmatter {
-                        id
-                        titleid
-                        title
-                        date(formatString: "MMMM DD, YYYY")
-                        path
-                        tags
-                        excerpt
-                    }
-                }
-            }
+  const blogPostTemplate = path.resolve(`src/templates/blog-post.js`);
+  const gql = graphql(`
+    {
+      allContentfulPost(filter: { node_locale: { eq: "en-US" } }) {
+        edges {
+          node {
+            id
+            date
+            path
+            title
+            excerpt
+            author
+            tags
+          }
         }
-    }`)
-    
-    gql.then(result => {
-        if(result.errors){
-            return Promise.reject(result.errors)
+      }
+    }
+  `);
+
+  gql.then(result => {
+    if (result.errors) {
+      return Promise.reject(result.errors);
+    }
+
+    const posts = result.data.allContentfulPost.edges;
+
+    createTagPages(createPage, posts);
+
+    posts.forEach(({ node }, index) => {
+      createPage({
+        path: node.path,
+        component: blogPostTemplate,
+        context: {
+          prev: index === 0 ? null : posts[index - 1].node,
+          next: index === posts.length - 1 ? null : posts[index + 1].node
         }
-
-        const posts = result.data.allMarkdownRemark.edges
-
-        createTagPages(createPage, posts)
-
-        posts.forEach(({node}, index) => {
-            createPage({
-                path: node.frontmatter.path,
-                component: blogPostTemplate,
-                context: {
-                    prev: index === 0 ? null : posts[index -1].node,
-                    next: index === (posts.length -1) ? null : posts[index + 1].node
-                }
-            });
-        });
+      });
     });
+  });
 
-    return gql
-}
+  return gql;
+};
