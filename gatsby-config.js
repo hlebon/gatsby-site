7d26c89ab97a2aaee42d298a33fe28db20316764
hlebon <hanslgarcia@gmail.com>
2018-09-15 23:01:15 -0500

    chore: gatsby contentful plugin added

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,7 +2,7 @@ module.exports = {
   pathPrefix: "/",
   siteMetadata: {
     title: `Fullstack blog`,
-    keywords: 'blog'
+    keywords: "blog"
   },
   plugins: [
     {
@@ -12,8 +12,8 @@ module.exports = {
           {
             resolve: `gatsby-remark-prismjs`,
             options: {
-              classPrefix: "language-",
-            },
+              classPrefix: "language-"
+            }
           },
           {
             resolve: `gatsby-source-filesystem`,
@@ -27,6 +27,13 @@ module.exports = {
     },
     {
       resolve: `gatsby-plugin-react-helmet`
+    },
+    {
+      resolve: `gatsby-source-contentful`,
+      options: {
+        spaceId: `1jeiz63qbsu5`,
+        accessToken: `a696a82c519e86c878766d551f6ecd4d0c5b3d3fbc4405eb3fd33b68deba9446`
+      }
     }
-  ],
-}
+  ]
+};
